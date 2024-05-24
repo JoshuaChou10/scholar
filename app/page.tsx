f564581e164e8c49a2d1b8dd6de5ea50c279e71f
Joshua Chou <joshuachou912@gmail.com>
2024-05-24 11:15:46 -0400

    Added course reminders

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,40 @@
 'use client'
 import Head from 'next/head'
 import { useState, useEffect } from 'react'
+import Select, {SingleValue} from 'react-select'
 import WeatherDisplay from '../components/weather'
 import ReminderList from '../components/reminderList'
 import Image from 'next/image'
 export default function Home() {
   const [currentDay, setCurrentDay] = useState<number>(0)
-  
   const [courses, setCourses] = useState<string[]>([])
   const [courseInputs, setCourseInputs] = useState<string[]>(['', '', '', ''])
-  // const [email,setEmail]=useState('')
-  
+  const [coursePage, setCoursePage] = useState<string>('')
+  const [reminders, setReminders] = useState<{ text: string; date: string, desc:string, course:string}[]>([])
+const [reminderText, setReminderText] = useState<string>('')
+const [reminderDate, setReminderDate] = useState<string>('')
+const [reminderDesc, setReminderDesc] = useState<string>('')
+const [reminderCourse, setReminderCourse] = useState<string>('')
+  interface optionType{
+    value:string;
+    label:string
+  }
   useEffect(() => {
     const today = new Date()
     setCurrentDay((today.getDate() % 2 === 0) ? 2 : 1)
 
-    
+    const savedReminders = JSON.parse(localStorage.getItem('reminders') || "[]")
+    if (savedReminders.length==0){
+        setReminders([{
+            text: 'Reminders will show up here',
+            date: new Date().toISOString().split('T')[0], // Set today's date as default
+            desc: 'Example reminder description',
+            course:''
+        }]);
+    }
+    else{
+        setReminders(savedReminders)
+    }
 
     const savedCourses = JSON.parse(localStorage.getItem('courses') || '[]')
     if (savedCourses.length > 0) {
@@ -29,6 +48,28 @@ export default function Home() {
 //     localStorage.setItem('email', email);
 //   }, [email]); 
   
+const addReminder = () => {
+  const newReminders = [...reminders, { text: reminderText, date: reminderDate, desc:reminderDesc, course:reminderCourse }]
+  setReminders(newReminders)
+  localStorage.setItem('reminders', JSON.stringify(newReminders))
+  setReminderText('')
+  setReminderDesc('')
+  setReminderDate('')
+  setReminderCourse('')
+}
+
+const deleteReminder = (index: number) => {
+  const newReminders = reminders.filter((_, i) => i !== index)
+  setReminders(newReminders)
+  localStorage.setItem('reminders', JSON.stringify(newReminders))
+}
+const editReminder = (index: number) => {
+  setReminderText(reminders[index].text)
+  setReminderDesc(reminders[index].desc)
+  setReminderDate(reminders[index].date)
+  deleteReminder(index)
+  
+}
 
   const handleCourseInputChange = (index: number, value: string) => {
     const newCourseInputs = [...courseInputs]
@@ -45,6 +86,13 @@ export default function Home() {
     setCourses([])
     localStorage.removeItem('courses')
   }
+  const getCourseReminders=(coursePage:string)=>{
+    return reminders.filter(r=>r.course==coursePage)
+  }
+  const getOtherReminders=()=>{
+    return reminders.filter(r=>r.course=='')
+  }
+
 
   const getOrderedCourses = () => {
     if (currentDay === 2 && courses.length === 4) {
@@ -52,7 +100,13 @@ export default function Home() {
     }
     return courses
   }
-
+  const options:optionType[] = [
+    { value: '', label: 'None' },
+    ...getOrderedCourses().map(course => ({ value: course, label: course }))
+];
+const handleSelectChange=(selectedOption:SingleValue<optionType>)=>{
+  setReminderCourse(selectedOption ? selectedOption.value : '')
+}
   return (
     <div className="flex min-h-screen bg-black text-gray-800">
         <Head>
@@ -80,9 +134,9 @@ export default function Home() {
                     <h2 className="text-2xl text-white mb-4">Schedule</h2>
                     {courses.length ? (
                        <div>
-                       <ul className="list-none p-0  ">
+                       <ul className=" w-1/2 list-none p-0  ">
                            {getOrderedCourses().map((course, index) => (
-                               <li key={index} className="  bg-blue-300 rounded-2xl p-2 mb-2 border border-blue-500 text-black text-center text-extrabold">
+                               <li key={index} onClick={()=>setCoursePage(course)} className="  bg-blue-300 rounded-2xl p-2 mb-2 border border-blue-500 text-black text-center text-extrabold">
                                    {course}
                                </li>
                            ))}
@@ -93,7 +147,18 @@ export default function Home() {
                        >
                            Edit Courses
                        </button>
-                       {/* <h3 className="text-xl text-white mb-4">Tomorrow is day {(currentDay === 1) ? 2 : 1}</h3> */}
+                       <section>
+                        <h2>{coursePage}</h2>
+    {getCourseReminders(coursePage).map((reminder,index)=>(
+      <li key={index}>
+        {reminder.text}
+      </li>
+
+    ))}
+    
+
+                       </section>
+
                    </div>
                    
                     ) : (
@@ -120,11 +185,46 @@ export default function Home() {
                 </div>
             </div>
 
-            
+            <h2 className="text-xl text-white mb-2 mt-5">Set a Reminder</h2>
+            <input
+                type="text"
+                className="w-full p-2 border border-gray-300 mb-2 text-black"
+                placeholder="Reminder title"
+                value={reminderText}
+                onChange={(e) => setReminderText(e.target.value)}
+            />
+            <input
+                type="text"
+                className="w-full p-2 border border-gray-300 mb-2 text-black"
+                placeholder="Reminder Description"
+                value={reminderDesc}
+                onChange={(e) => setReminderDesc(e.target.value)}
+            />
+            <input
+                type="date"
+                className="w-full p-2 border border-gray-300 mb-2 text-black"
+                value={reminderDate}
+                onChange={(e) => setReminderDate(e.target.value)}
+            />
+            <Select 
+            options={options} 
+            value={options.find(option => option.value === reminderCourse)} 
+            onChange={handleSelectChange}                   
+       />
+            <button
+                className="bg-blue-500 text-white py-2 px-4 mb-4"
+                onClick={addReminder}
+            >
+                
+                Add Reminder
+            </button>
             <h2 className="text-xl text-white mb-2">Current Reminders</h2>
-            <ReminderList/>
+            <ReminderList reminders={getOtherReminders}/>
         </main>
     </div>
 );
+
                             }
+
+
                          
--- components/reminderList.tsx
+++ components/reminderList.tsx
@@ -1,45 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import editReminder from '../app/page'
+import deleteReminder from '../app/page'
 
 
-// Assuming your reminder object might look something like this
-// reminder = { text: 'Doctor Appointment', date: '2024-01-01', desc: 'Annual check-up at 3 PM' }
-
-export default function ReminderList() {
-    const [reminders, setReminders] = useState<{ text: string; date: string, desc:string}[]>([])
-const [reminderText, setReminderText] = useState<string>('')
-const [reminderDate, setReminderDate] = useState<string>('')
-const [reminderDesc, setReminderDesc] = useState<string>('')
+export default function ReminderList(reminders) {
+    
     // State to track the visible description; null initially means all are collapsed
     const [visibleDescriptionIndex, setVisibleDescriptionIndex] = useState<number | null>(null);
-useEffect(()=>{
-    const savedReminders = JSON.parse(localStorage.getItem('reminders') || "[]")
-    if (savedReminders.length==0){
-        setReminders([{
-            text: 'Reminders will show up here',
-            date: new Date().toISOString().split('T')[0], // Set today's date as default
-            desc: 'Example reminder description'
-        }]);
-    }
-    else{
-        setReminders(savedReminders)
-    }
-},[])
+
     // Toggle function to show/hide descriptions
     
-  const addReminder = () => {
-    const newReminders = [...reminders, { text: reminderText, date: reminderDate, desc:reminderDesc }]
-    setReminders(newReminders)
-    localStorage.setItem('reminders', JSON.stringify(newReminders))
-    setReminderText('')
-    setReminderDesc('')
-    setReminderDate('')
-  }
-
-  const deleteReminder = (index: number) => {
-    const newReminders = reminders.filter((_, i) => i !== index)
-    setReminders(newReminders)
-    localStorage.setItem('reminders', JSON.stringify(newReminders))
-  }
 
     const toggleDescription = (index: number) => {
         setVisibleDescriptionIndex(visibleDescriptionIndex === index ? null : index);
@@ -53,70 +23,49 @@ useEffect(()=>{
     };
     return (
         <div>
-        <h2 className="text-xl text-white mb-2 mt-5">Set a Reminder</h2>
-            <input
-                type="text"
-                className="w-full p-2 border border-gray-300 mb-2 text-black"
-                placeholder="Reminder title"
-                value={reminderText}
-                onChange={(e) => setReminderText(e.target.value)}
-            />
-            <input
-                type="text"
-                className="w-full p-2 border border-gray-300 mb-2 text-black"
-                placeholder="Reminder Description"
-                value={reminderDesc}
-                onChange={(e) => setReminderDesc(e.target.value)}
-            />
-            <input
-                type="date"
-                className="w-full p-2 border border-gray-300 mb-2 text-black"
-                value={reminderDate}
-                onChange={(e) => setReminderDate(e.target.value)}
-            />
-            <button
-                className="bg-blue-500 text-white py-2 px-4 mb-4"
-                onClick={addReminder}
-            >
-                
-                Add Reminder
-            </button>
-        <ul className="list-none p-0">
+       
+            <ul className="list-none p-0">
     
-            {reminders.map((reminder, index:number) => (
-                <li
-                    key={index}
-                    className="p-2 mb-2 border border-blue-500/50 rounded-2xl text-white bg-transparent shadow-lg shadow-blue-500/50 transition-all duration-300"
+    {reminders.map((reminder, index:number) => (
+        <li
+            key={index}
+            className="p-2 mb-2 border border-blue-500/50 rounded-2xl text-white bg-transparent shadow-lg shadow-blue-500/50 transition-all duration-300"
+        >
+    
+            <div className="flex justify-between items-center">
+                {/* Clickable title */}
+                <div className={`flex-grow cursor-pointer flex items-center ${(getDaysUntil(reminder.date)<2? 'text-red': 'text-white')}`} onClick={() => toggleDescription(index)}>
+                    {reminder.text} {reminder.date && `-  ${(getDaysUntil(reminder.date))<=0? 'Due Today':`Due in ${getDaysUntil(reminder.date)} Days`}`}
+                    <svg className="fill-current text-white w-4 h-4 ml-2 transform transition-transform duration-300" style={{ transform: visibleDescriptionIndex === index ? 'rotate(180deg)' : 'rotate(0deg)' }} viewBox="0 0 20 20">
+                        <path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
+                    </svg>
+                </div>
+                {/* Delete button */}
+                <button
+                    className="bg-red-500 rounded-full text-white py-1 px-2 mx-3"
+                    onClick={() => deleteReminder(index)}
                 >
-                    {/* Container for title and delete button */}
-                    <div className="flex justify-between items-center">
-                        {/* Clickable title */}
-                        <div className={`flex-grow cursor-pointer flex items-center ${(getDaysUntil(reminder.date)<2? 'text-red': 'text-white')}`} onClick={() => toggleDescription(index)}>
-                            {reminder.text} {reminder.date && `-  ${(getDaysUntil(reminder.date))<=0? 'Due Today':`Due in ${getDaysUntil(reminder.date)} Days`}`}
-                            <svg className="fill-current text-white w-4 h-4 ml-2 transform transition-transform duration-300" style={{ transform: visibleDescriptionIndex === index ? 'rotate(180deg)' : 'rotate(0deg)' }} viewBox="0 0 20 20">
-                                <path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
-                            </svg>
-                        </div>
-                        {/* Delete button */}
-                        <button
-                            className="bg-red-500 rounded-full text-white py-1 px-2"
-                            onClick={() => deleteReminder(index)}
-                        >
-                            Delete
-                        </button>
-                    </div>
-                    
-                    {/* Conditionally displayed description below the title and button */}
-                    {visibleDescriptionIndex === index && (
-                        <div className="text-blue-500 p-2 mt-2 transition-all duration-300" style={{ maxHeight: visibleDescriptionIndex === index ? '100px' : '0', overflow: 'hidden' }}>
-                            {reminder.desc}
-                            <br />
-                            To be completed by: {(reminder.date)? reminder.date:"Whenever you want"}
-                        </div>
-                    )}
-                </li>
-            ))}
-        </ul>
+                    Delete
+                </button>
+                <button
+                    className="bg-blue-500 rounded-full text-white py-1 px-2"
+                    onClick={() => editReminder(index)}
+                >
+                    Edit
+                </button>
+            </div>
+            
+            {/* Conditionally displayed description below the title and button */}
+            {visibleDescriptionIndex === index && (
+                <div className="text-blue-500 p-2 mt-2 transition-all duration-300" style={{ maxHeight: visibleDescriptionIndex === index ? '100px' : '0', overflow: 'hidden' }}>
+                    {reminder.desc}
+                    <br />
+                    To be completed by: {(reminder.date)? reminder.date:"Whenever you want"}
+                </div>
+            )}
+        </li>
+    ))}
+    </ul>
         </div>
     );
      
